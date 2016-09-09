a84f97286d0ebb4fcab98a1c62d43e1d966205f0
TANABE Takashi <narinari.t@gmail.com>
2016-09-09 14:12:24 +0900

    Follow Bootstrap4.alpha.4

--- dev/GenerateBootstrap.hs
+++ dev/GenerateBootstrap.hs
@@ -7,7 +7,7 @@
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 import           Control.Monad
 import           CssParserLite
-import           Data.List        (nub, sort)
+import           Data.List        (elem, nub, sort)
 import           Data.Monoid
 import qualified Data.Text        as T
 import qualified Debug.Trace      as D
@@ -18,7 +18,7 @@ default (T.Text, Int)
 
 main :: IO ()
 main = shelly $ do
-  styleSheet <- print_stdout False $ run "curl" ["https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css"]
+  styleSheet <- print_stdout False $ run "curl" ["https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css"]
   let classes = foldMap collectClassSelecter <$> parse css "parse selecter" styleSheet
       classNames = (nub.sort.fmap (mkClassName.T.pack.className)) <$> classes
   echoFileLead
@@ -33,8 +33,8 @@ main = shelly $ do
 
     mkClassName line =
       let parts = T.split (=='-') line
-          remain = T.toTitle <$> tail parts
-          methodName = T.intercalate T.empty $ head parts : remain
+          capitalized = T.toTitle <$> tail parts
+          methodName = escapeKeywords $ T.intercalate T.empty $ head parts : capitalized
       in methodName <> " :: ClassName\n"
           <> methodName <> " = className \"" <> line <> "\"\n"
 
@@ -51,3 +51,10 @@ main = shelly $ do
 
     echoResult (Right a) = void (sequenceA $ echo <$> a)
     echoResult (Left err) = echo_err $ T.pack $ show err
+
+    escapeKeywords word =
+      if word `elem` reserved then word <> "_" else word
+      where
+      reserved =
+        [ "as", "case", "of", "class", "data", "default", "derive", "do", "forall", "foreign", "hiding", "if", "then", "else"
+        , "import", "infix", "infixl", "infixr", "instance", "let", "in", "module", "newtype", "type", "where" ]
